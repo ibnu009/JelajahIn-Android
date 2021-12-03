0d8605b516445174a791d175eb548fcabd8423ad
ibnu009 <ibnubatutah001@gmail.com>
2021-12-03 21:39:03 +0700

    Testtt

--- app/src/main/java/com/ibnu/jelajahin/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/ibnu/jelajahin/ui/home/HomeViewModel.kt
@@ -3,7 +3,6 @@ package com.ibnu.jelajahin.ui.home
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 
-@HiltViewModel
 class HomeViewModel : ViewModel() {
 
 }
